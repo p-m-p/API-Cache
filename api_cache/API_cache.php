f4bf1599c4519a95e2c37127d1edcc5721d0dd6a
Eddie Lee <eddielee6@me.com>
2012-10-10 23:14:34 +0100

    Updated `_update_cache` function to lock cache before making the API call
    See issue: https://github.com/p-m-p/API-Cache/issues/1

--- api_cache/API_cache.php
+++ api_cache/API_cache.php
@@ -44,18 +44,22 @@ class API_cache {
    * Makes the api call and updates the cache 
    */
   private function _update_cache () {
-    //Attempt to get new API data
-    $apiData = @file_get_contents ($this->_api_call);
-    //Update cache if API call was successful
-    if($apiData !== FALSE) {
-      $fp = fopen($this->_cache_file, 'w+'); // open or create cache
-      if ($fp) {
-        if (flock($fp, LOCK_EX)) {
+    $fp = fopen($this->_cache_file, 'a+'); // open or create cache
+    if ($fp) {
+      if (flock($fp, LOCK_EX)) {
+        //Attempt to get new API data
+        $apiData = @file_get_contents ($this->_api_call);
+        //Update cache if API call was successful
+        if($apiData !== FALSE) {
+          //Clear cache
+          fseek($fp, 0);
+          ftruncate($fphandle, filesize($this->_cache_file));
+          //Update cache
           fwrite($fp, $apiData);
-          flock($fp, LOCK_UN);
         }
-        fclose($fp);
-      }   
+        flock($fp, LOCK_UN);
+      }
+    fclose($fp);
     }
   }
 }
